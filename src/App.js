f4acfb93a045b2c53893a5e7f7d2b04ec0e0bbb3
hargodwn <97815831+hargodwn@users.noreply.github.com>
2022-01-16 14:39:00 +0700

    first submit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+import "./Assets/App.scss";
+
+function App() {
+  return (
+    <div className="container">
+      <div className="navbar">header</div>
+
+      <div className="section">
+        <div className="card section-body-1">1</div>
+        <div className="card section-body-2">2</div>
+        <div className="card section-body-3">3</div>
+        <div className="card section-body-4">4</div>
+        <div className="card section-body-5">5</div>
+        <div className="card section-body-6">6</div>
+      </div>
+      <div className="body">
+      <div className="body-Anak Body-1">1</div>
+      <div className="body-Anak Body-1">2</div>
+      <div className="body-Anak Body-1">3</div>
+    </div>
+    </div>
+
+  );
+}
+
+export default App;
